ad068463224182abec64ee56105bf71001cae0c3
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-06-14 18:45:34 -0400

    added pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './components/Home'
+import About from './components/About'
+import Interests from './components/Interests'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import WorkHistory from './components/WorkHistory'
+import Projects from './components/Projects'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      APP
     </>
   )
 }
---
+++ src/components/WorkHistory.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WorkHistory() {
+  return <div>WorkHistory</div>;
+}
